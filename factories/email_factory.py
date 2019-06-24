ae22340f7c1aa63aac6fe0186d54f68394b56bbc
roelo34 <roeldeking@gmail.com>
2019-06-24 14:38:14 +0200

    Create mails from treatments added

--- factories/email_factory.py
+++ factories/email_factory.py
@@ -1,12 +1,19 @@
 from models.email_model import EmailModel
 from models.identity_model import IdentityModel
+from models.treatment_model import TreatmentModel
+from factories.identity_factory import IdentityFactory
 from util.singleton import Singleton
+from datetime import datetime
 import mailbox
 
 class EmailFactory(metaclass=Singleton):
 
-    def create_email(self, sender: IdentityModel, recipient: IdentityModel, subject, content):
+    def create_email_from_treatment(self, sender: IdentityModel, recipient: IdentityModel, treatment: TreatmentModel):
+        appointment_date = IdentityFactory.random_date(datetime(day=1, month=1, year=1991), datetime(day=1, month=1, year=2020))
         model = EmailModel()
-        model.set_to_from_identymodel(sender)
-        model.set_from_from_identymodel(recipient)
-        
+        model.set_to_from_identymodel(recipient)
+        model.set_from_from_identymodel(sender, True)
+        model.subject = "Uw behandeling {0} op {1} ".format(treatment.treatmentDesc, appointment_date)
+        model.content = "Beste {0},\n\nUw behandeling {1} op {2} staat in de planning.\n\nMet vriendelijke groet,\n\nKiesMondzorg".format(
+            recipient.lastName, treatment.treatmentDesc, appointment_date)
+        return model
--- models/email_model.py
+++ models/email_model.py
@@ -3,7 +3,7 @@ from random import choice
 
 
 class EmailModel:
-    _email_domains = ["@gmail.com", "@yandex.ru", "@kiesmondzorg.nl", "@live.nl"]
+    _email_domains = ["gmail.com", "yandex.ru", "habbo_hotel.nl", "live.nl"]
     email_to: str
     email_from: str
     email_cc: str
@@ -11,7 +11,10 @@ class EmailModel:
     content: str
 
     def set_to_from_identymodel(self, model: IdentityModel):
-        self.email_to = model.full_name + choice(self._email_domains)
+        self.email_to = "{0}@{1}".format("".join([x for x in model.full_name if x != " "]), choice(self._email_domains))
     
-    def set_from_from_identymodel(self, model: IdentityModel):
-        self.email_from = model.full_name + choice(self._email_domains)
+    def set_from_from_identymodel(self, model: IdentityModel, is_medewerker: bool = False):
+        if is_medewerker:
+            self.email_from = "{0}@kiesmondzorg.nl".format("".join([x for x in model.full_name if x != " "]))
+        else:
+            self.email_from = self.email_from = "{0}@{1}".format("".join([x for x in model.full_name if x != " "]), choice(self._email_domains))
