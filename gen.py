d0bd72cebe93c9f9916e0c37e2e9921a638499d3
roelo34 <roeldeking@gmail.com>
2019-06-24 14:45:09 +0200

    Added mbox creation functionality

--- gen.py
+++ gen.py
@@ -1,7 +1,11 @@
 from docx import Document
 from factories.identity_factory import IdentityFactory
 from models.identity_model import IdentityModel
+from factories.email_factory import EmailFactory
+from models.email_model import EmailModel
+from repositories.treatment_repository import TreatmentRepository
 import random
+import mailbox
 
 
 def create_doc(identity: IdentityModel) -> None:
@@ -32,9 +36,29 @@ def create_doc(identity: IdentityModel) -> None:
 
     document.save("dossier {0}.docx".format(patient_nr))
 
+def create_mailbox(email: EmailModel) -> None:
+    mbox = mailbox.mbox("mailbox.mbox")
+    mbox.lock()
+    try:
+        msg = mailbox.mboxMessage()
+        msg.set_unixfrom('author Sat Feb  7 01:05:34 2009')
+        msg['From'] = email.email_from
+        msg['To'] = email.email_to
+        msg['Subject'] = email.subject
+        msg.set_payload(email.content)
+        mbox.add(msg)
+        mbox.flush()
+    finally:
+        mbox.unlock()
 
 if __name__ == "__main__":
-    factory = IdentityFactory()
+    identity_factory = IdentityFactory()
+    email_factory = EmailFactory()
+    treatment_repos = TreatmentRepository()
+    identities = [identity_factory.get_random_identity() for x in range(0, 1000)]
 
     for i in range(1, 20):
-        create_doc(factory.get_random_identity())
+        patient = random.choice(identities)
+        doctor = random.choice(identities)
+        # create_doc(patient)
+        create_mailbox(email_factory.create_email_from_treatment(doctor, patient, treatment_repos.get_random_treatment()))
