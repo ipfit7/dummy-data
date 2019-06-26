37b77eae15d37edf9f1e4f1beeed59391b5041c3
roelo34 <roeldeking@gmail.com>
2019-06-26 14:10:45 +0200

    Incorperated the new doc factory

--- gen.py
+++ gen.py
@@ -1,40 +1,14 @@
-from docx import Document
+from factories.doc_factory import DocFactory
 from factories.identity_factory import IdentityFactory
 from models.identity_model import IdentityModel
 from factories.email_factory import EmailFactory
 from models.email_model import EmailModel
+from models.treatment_model import TreatmentModel
 from repositories.treatment_repository import TreatmentRepository
+from repositories.photo_repository import PhotoRepository
 import random
 import mailbox
-
-
-def create_doc(identity: IdentityModel) -> None:
-    # Placeholder
-    patient_nr = random.randint(10000, 99999)
-
-    #create document
-    document = Document()
-    document.add_heading('Patient Dossier van ' + identity.full_name, 0)
-
-    # Create table
-    patientInfo = document.add_table(rows=5, cols=2)
-
-    desc_cells = patientInfo.columns[0].cells
-    desc_cells[0].text = "Patient nummer"
-    desc_cells[1].text = "Naam"
-    desc_cells[2].text = "Geboorte datum"
-    desc_cells[3].text = "Woonplaats"
-    desc_cells[4].text = "BSN"
-
-    info_cells = patientInfo.columns[1].cells
-
-    info_cells[0].text = str(patient_nr)
-    info_cells[1].text = identity.full_name
-    info_cells[2].text = str(identity.dateOfBirth)
-    info_cells[3].text = identity.placeOfResidence
-    info_cells[4].text = str(identity.bsn)
-
-    document.save("dossier {0}.docx".format(patient_nr))
+from pathlib import Path
 
 def create_mailbox(email: EmailModel) -> None:
     mbox = mailbox.mbox("mailbox.mbox")
@@ -53,12 +27,16 @@ def create_mailbox(email: EmailModel) -> None:
 
 if __name__ == "__main__":
     identity_factory = IdentityFactory()
+    treatment_repo = TreatmentRepository()
+    doc_factory = DocFactory()
     email_factory = EmailFactory()
     treatment_repos = TreatmentRepository()
-    identities = [identity_factory.get_random_identity() for x in range(0, 1000)]
-
-    for i in range(1, 20):
-        patient = random.choice(identities)
-        doctor = random.choice(identities)
-        # create_doc(patient)
-        create_mailbox(email_factory.create_email_from_treatment(doctor, patient, treatment_repos.get_random_treatment()))
+    patients = [identity_factory.get_random_identity() for x in range(0, 50)]
+    doctors = [identity_factory.get_random_identity() for y in range(0,130)]
+    
+    for patient in patients:
+        doctor = random.choice(patients)
+        treatments = [treatment_repo.get_random_treatment() for n in range(0, random.randint(0, 21))]
+        doc_factory.create_doc(patient, treatments).save('dossier {0}.docx'.format(patient.bsn))
+        for treatment in treatments:
+            create_mailbox(email_factory.create_email_from_treatment(doctor, patient, treatment))
