b1d2d49025d252ff89c3e4e4744bd22c04d1587d
roelo34 <roeldeking@gmail.com>
2019-06-28 13:06:42 +0200

    Integrated data access layer

--- gen.py
+++ gen.py
@@ -6,6 +6,7 @@ from models.email_model import EmailModel
 from models.treatment_model import TreatmentModel
 from repositories.treatment_repository import TreatmentRepository
 from repositories.photo_repository import PhotoRepository
+from repositories.sql_repository import SQLRepository
 import random
 import mailbox
 from pathlib import Path
@@ -28,15 +29,19 @@ def create_mailbox(email: EmailModel) -> None:
 if __name__ == "__main__":
     identity_factory = IdentityFactory()
     treatment_repo = TreatmentRepository()
+    sql_repo = SQLRepository()
     doc_factory = DocFactory()
     email_factory = EmailFactory()
     treatment_repos = TreatmentRepository()
     patients = [identity_factory.get_random_identity() for x in range(0, 50)]
     doctors = [identity_factory.get_random_identity() for y in range(0,130)]
+    sql_repo.insert_treatments(treatment_repo.get_treatment(n) for n in range(10,21))
     
     for patient in patients:
         doctor = random.choice(patients)
+        sql_repo.insert_patients(patient)
         treatments = [treatment_repo.get_random_treatment() for n in range(0, random.randint(0, 21))]
         doc_factory.create_doc(patient, treatments).save('dossier {0}.docx'.format(patient.bsn))
+        sql_repo.insert_history(doctor, patient, treatments)
         for treatment in treatments:
             create_mailbox(email_factory.create_email_from_treatment(doctor, patient, treatment))
