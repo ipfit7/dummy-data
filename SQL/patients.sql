bda69eeb3f56de074115d23ed00f87a7fd72ccb6
roelo34 <roeldeking@gmail.com>
2019-06-29 21:47:31 +0200

    Added patientID column to the patients table

--- SQL/patients.sql
+++ SQL/patients.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `patients` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `patientID` int(11) NOT NULL,
+  `firstName` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `middleName` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `lastName` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `bsn` int(11) NOT NULL,
@@ -8,4 +9,4 @@ CREATE TABLE `patients` (
   `address` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `placeOfResidence` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
