876181dbdbe284e38d624d1cd7b865cdcc8e030d
roelo34 <roeldeking@gmail.com>
2019-07-01 10:23:38 +0200

    Added create statement for accounting db

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE `patientinfo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE `boekhouding` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
